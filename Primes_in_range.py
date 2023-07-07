32c99183322a3b29af4730db51018c96250112b7
Rakeshnelli <98540507+Rakeshnelli@users.noreply.github.com>
2023-07-07 13:37:53 +0530

    n= int(input()) m= int(input()) prime_numbers = [] for i in range(n,m+1):     if i > 1:         is_prime = True         for j in range(2, int(i ** 0.5) + 1):             if i % j == 0:                 is_prime = False                 break         if is_prime:             prime_numbers.append(i) print(len(prime_numbers))

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,13 +1,13 @@
 n= int(input())
 m= int(input())
 prime_numbers = []
-for num in range(n,m+1):
-    if num > 1:
+for i in range(n,m+1):
+    if i > 1:
         is_prime = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
                 is_prime = False
                 break
         if is_prime:
-            prime_numbers.append(num)
+            prime_numbers.append(i)
 print(len(prime_numbers))
