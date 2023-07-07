568c9822f113a7710ad31d326d1991d0c3e5e1a4
Rakeshnelli <98540507+Rakeshnelli@users.noreply.github.com>
2023-07-07 12:09:03 +0530

    num = int(input())
    if num <= 0:
        print("Not Ugly Number")
    else:
        while num % 2 == 0:
            num //= 2
        while num % 3 == 0:
            num //= 3
        while num % 5 == 0:
            num //= 5
        if num == 1:
            print("Ugly Number")
        else:
            print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+if num <= 0:
+    print("Not Ugly Number")
+else:
+    while num % 2 == 0:
+        num //= 2
+    while num % 3 == 0:
+        num //= 3
+    while num % 5 == 0:
+        num //= 5
+    if num == 1:
+        print("Ugly Number")
+    else:
+        print("Not Ugly Number")
