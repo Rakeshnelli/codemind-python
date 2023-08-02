19d6a9052985cfd8010f481a372ce557fd2c09a2
Rakeshnelli <98540507+Rakeshnelli@users.noreply.github.com>
2023-08-02 11:56:07 +0530

    n=int(input()) c=0 for i in range(n):     m=int(input())     l=list(map(int,input().split()))     for j in range(1,m):         if l[j-1]>l[j]:             c+=1     if c==0:         print(c)     else:         x=max(l)         y=min(l)         print(x-y)

--- Optimal_Sorting.py
+++ Optimal_Sorting.py
@@ -1,14 +1,14 @@
 n=int(input())
 c=0
 for i in range(n):
-    a=int(input())
+    m=int(input())
     l=list(map(int,input().split()))
-    for j in range(1,a):
+    for j in range(1,m):
         if l[j-1]>l[j]:
             c+=1
     if c==0:
         print(c)
     else:
-        m=max(l)
-        n=min(l)
-        print(m-n)
+        x=max(l)
+        y=min(l)
+        print(x-y)
